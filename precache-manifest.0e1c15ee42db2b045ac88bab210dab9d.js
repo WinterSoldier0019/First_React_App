ca07db6ff09fc97bd36a7c599a5d85741190ab79
Arjun Yadav <arjunyadav001@gmail.com>
2019-06-16 02:10:37 +0530

    Updates

---
+++ precache-manifest.0e1c15ee42db2b045ac88bab210dab9d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61170fece40c9d8621b1387f8aeb9950",
+    "url": "/First_React_App/index.html"
+  },
+  {
+    "revision": "8dc59b6cb6e904b992cc",
+    "url": "/First_React_App/static/css/main.e435e2a9.chunk.css"
+  },
+  {
+    "revision": "e90574d74274c7fb4d49",
+    "url": "/First_React_App/static/js/2.c39fed63.chunk.js"
+  },
+  {
+    "revision": "8dc59b6cb6e904b992cc",
+    "url": "/First_React_App/static/js/main.ac0e89c4.chunk.js"
+  },
+  {
+    "revision": "38451b159d8d05366cb4",
+    "url": "/First_React_App/static/js/runtime~main.084ac97e.js"
+  }
+]);
